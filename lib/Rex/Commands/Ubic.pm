08a3f122f6346c99358c239865a2704ca9c98096
Fayland Lam <fayland@gmail.com>
2017-02-21 16:15:40 +0800

    not useful, depreacated

--- lib/Rex/Commands/Ubic.pm
+++ lib/Rex/Commands/Ubic.pm
@@ -1,8 +1,50 @@
 package Rex::Commands::Ubic;
 
 use strict;
-use 5.008_005;
+use warnings;
+
 our $VERSION = '0.01';
+
+require Rex::Exporter;
+use base qw(Rex::Exporter);
+use vars qw(@EXPORT);
+use Carp;
+
+use Rex::Logger;
+use Rex::Commands;
+use Rex::Commands::Run;
+use Rex::Commands::Fs;
+use Rex::Commands::File;
+
+# rexify --use Rex::Misc::ShellBlock
+# use Rex::Misc::ShellBlock;
+# use JSON;
+
+@EXPORT = qw(ubic ubic_status ubic_service);
+
+sub ubic_status {
+    my $ret = run "ubic status";
+    my @lines = split(/[\r\n]+/, $ret);
+}
+
+# sub ubic_status {
+#     my $data = shell_block <<'CODE';
+# #!/usr/bin/env perl
+
+# use strict;
+# use warnings;
+# use JSON;
+# use Ubic;
+
+# CODE
+# }
+
+sub ubic_service {
+    my ($name, %opts) = @_;
+
+
+}
+
 
 1;
 __END__
@@ -11,11 +53,19 @@ __END__
 
 =head1 NAME
 
-Rex::Commands::Ubic - Blah blah blah
+Rex::Commands::Ubic - Rex joins Ubic
 
 =head1 SYNOPSIS
 
   use Rex::Commands::Ubic;
+
+=head1 NOTE
+
+Do
+
+    rexify --use Rex::Misc::ShellBlock
+
+to install the requirement first
 
 =head1 DESCRIPTION
 
